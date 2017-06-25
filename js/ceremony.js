0c2ef6f722a372864ef1232651165a23d22e60dc
Samantha-Rae Adamson <samyadamson@gmail.com>
2017-06-25 20:25:26 +0100

    updated all navbars to include hotel info, home page now has hotel infocard, our party page has updated information, ceremony page now has hide/show divs for events

---
+++ js/ceremony.js
@@ -0,0 +1,48 @@
+// $("#firstBusBox").click(function(){
+//   $("#firstBusInfo").show();
+// });
+
+function firstBus() {
+  var firstBus = document.getElementById('firstBusCard');
+  var ceremony = document.getElementById('ceremonyCard');
+  var reception = document.getElementById('receptionCard');
+  var lastBus = document.getElementById('lastBusCard');
+
+    if (firstBus.style.display === 'none') {
+        firstBus.style.display = 'block';
+        ceremony.style.display = 'none';
+        reception.style.display = 'none';
+        lastBus.style.display = 'none';
+
+    } else {
+        firstBus.style.display = 'none';
+        ceremony.style.display = 'block';
+        reception.style.display = 'none';
+        lastBus.style.display = 'none';
+    }
+}
+
+
+var event_list = ["firstBusCard", "ceremonyCard", "receptionCard", "lastBusCard"];
+
+function onShowEventInfo(index){
+  for(var i=0;i<event_list.length;i++)
+    try {
+      document.getElementById(event_list[i]).style.display = "none";
+    }
+    catch(e) {
+      console.error(e);
+      throw new Error("No element found with id "+event_list[i]+" in event_list.")
+    }
+  try {
+    document.getElementById(event_list[index]).style.display = "block";
+  }
+  catch(e) {
+    console.error(e);
+    throw new Error("No element found with id "+event_list[index]+" in event_list.")
+  }
+}
+
+setTimeout(function(){
+  onShowEventInfo(1);
+},50);
